be7ce0a4a78eb7a31f9157574faff365af5f6ec2
sbarbett <shane.barbetta@neustar.biz>
2014-11-04 14:26:12 -0500

    adding some test scenarios

---
+++ src/scenarios/scenario1.py
@@ -0,0 +1,29 @@
+# This client is designed to easily interact with Webmetrics' API.
+# Copyright (C) 2014  Shane Barbetta
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+__author__ = 'Shane Barbetta'
+
+import wm_api_client
+import sys
+
+if len(sys.argv) != 3:
+	raise Exception('Expected use: python sample.py username api_key')
+	
+username = sys.argv[1]
+api_key = sys.argv[2]
+
+c = wm_api_client.ApiClient(username, api_key)
+
+print 'get services %s ' % c.get_services()
