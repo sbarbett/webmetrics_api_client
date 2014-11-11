b2c81c976e69668eabfc33bc5519410d252b8c03
sbarbett <shane.barbetta@neustar.biz>
2014-11-11 16:57:15 -0500

    support for mutli-value GET parameters and alert API additions

--- src/connection.py
+++ src/connection.py
@@ -25,46 +25,46 @@ import json
 # store the URL and signature as state
 
 class ApiConnection:
-	def __init__(self):
-		self.username = ''
-		self.sig = ''
-		self.api_key = ''
-		self.base_url = 'https://api.webmetrics.com/v2/?'
-		
-	# Authentication
-	# The ability to created a signature hash value and retain it for
-	# its +/- 10 minute duration, while dynamically refreshing the sig
-	# once it becomes stale. If a request fails, it should try again
-	# with a new auth token.
-	def auth(self, username, api_key):
-		self.username = username
-		self.api_key = api_key
-		timestamp = str(int(time.time()))
-		self.sig = base64.b64encode(hashlib.sha1(username + api_key + timestamp).digest())
-	
-	def _refresh(self, method):
-		self.auth(self.username, self.api_key)
-		return self._do_call(method, False)
-	
-	# Requests
-	# Methods for accessing the API using Python's native urllib2
-	# libraries. Use the auth methods to verify the stored signature 
-	# and refresh if needed.
-	def get(self, method):
-		return self._do_call(method)
-	
-	def _do_call(self, method, retry=True):
-		base_query = { 'username' : self.username, 'sig' : self.sig, 'format' : 'json' }
-		base_query.update(method)
-		request = self.base_url + urllib.urlencode(base_query)
-		# print request
-		response = urllib2.urlopen(request)
-		data = json.load(response)
-		# print json.dumps(data)
-		time.sleep(3)
-		if data['stat'] == 'fail' and retry == False:
-			raise Exception("Authentication failed.")
-		elif data['stat'] == 'fail':
-			return self._refresh(method)
-		else:
-			return json.dumps(data)
+    def __init__(self):
+        self.username = ''
+        self.sig = ''
+        self.api_key = ''
+        self.base_url = 'https://api.webmetrics.com/v2/?'
+        
+    # Authentication
+    # The ability to created a signature hash value and retain it for
+    # its +/- 10 minute duration, while dynamically refreshing the sig
+    # once it becomes stale. If a request fails, it should try again
+    # with a new auth token.
+    def auth(self, username, api_key):
+        self.username = username
+        self.api_key = api_key
+        timestamp = str(int(time.time()))
+        self.sig = base64.b64encode(hashlib.sha1(username + api_key + timestamp).digest())
+
+    def _refresh(self, method):
+        self.auth(self.username, self.api_key)
+        return self._do_call(method, False)
+
+    # Requests
+    # Methods for accessing the API using Python's native urllib2
+    # libraries. Use the auth methods to verify the stored signature 
+    # and refresh if needed.
+    def get(self, method):
+        return self._do_call(method)
+
+    def _do_call(self, method, retry=True):
+        base_query = { 'username' : self.username, 'sig' : self.sig, 'format' : 'json' }
+        base_query.update(method)
+        request = self.base_url + urllib.urlencode(base_query, doseq=True)
+        # print request
+        response = urllib2.urlopen(request)
+        data = json.load(response)
+        # print json.dumps(data)
+        time.sleep(3)
+        if data['stat'] == 'fail' and retry == False:
+            raise Exception("Authentication failed.")
+        elif data['stat'] == 'fail':
+            return self._refresh(method)
+        else:
+            return json.dumps(data)
--- src/wm_api_client.py
+++ src/wm_api_client.py
@@ -21,169 +21,233 @@ import json
 # client containing different API services
 
 class ApiClient:
-	def __init__(self, username, api_key):
-		"""Initialize Webmetrics' API Client.
-		
-		Arguments:
-		username -- The username of the user
-		api_key -- The API key of the user
-		
-		"""
-		self.api_connection = connection.ApiConnection()
-		self.api_connection.auth(username, api_key)
-		
-	# General
-	# get account services
-	def get_services(self):
-		"""Get a list of service names and ids for an account."""
-		query = { 'method' : 'maintenance.getServices' }
-		return self.api_connection.get(query)
-	
-	# get account notepad
-	def get_notepad(self):
-		"""Returns the notepad of a given account."""
-		query = { 'method' : 'maintenance.getNotepad' }
-		return self.api_connection.get(query)
-	
-	# set account notepad
-	def set_notepad(self, notepad):
-		"""Sets the notepad of an account.
-		
-		Arguments:
-		notepad -- The notepad contents you wish to set.
-		
-		"""
-		query = { 'method' : 'maintenance.setNotepad', 'notepad' : notepad }
-		return self.api_connection.get(query)
-		
-	# get the 'type' of a service by id
-	def get_service_type(self, service_id):
-		"""Given a service id, returns the monitoring type.
-		
-		Arguments:
-		service_id -- The id of the service you want to get the 
-		              type of.
-					 
-		"""
-		query = { 'method' : 'maintenance.getServiceType', 'serviceid' : service_id }
-		return self.api_connection.get(query)
-		
-	# Admin
-	# create a new monitoring service
-	def add_new_service(self, service_name, service_type):
-		"""Add a new monitoring service to the specified Webmetrics
-		account. A trial is created for customers not on metered.
-		
-		Arguments:
-		service_name -- The name to give the new service. Service
-		                names can include alphanumeric, hyphen and
-					    underscore characters.
-		service_type -- The type of service. You can use the following:
-		                APPLICATION
-					    DNS
-					    FTP
-					    PING
-					    POP
-					    PORT
-					    SMTP
-					    STREAM
-					    WEBSERVICE
-					    WEBSITE
-					    WSTRANS		
-	
-		"""
-		query = { 'method' : 'maintenance.addNewService', 'servicename' : service_name, 'servicetype' : service_type.lower() }
-		return self.api_connection.get(query)
-	
-	# change your user password
-	def change_password(self, new_password):
-		"""Sets the password for a given username.
-		
-		Arguments:
-		new_password -- The new password.
-		
-		"""
-		query = { 'method' : 'maintenance.changePassword', 'newpassword' : new_password }
-		return self.api_connection.get(query)
-	
-	# rename an existing service
-	def rename_service(self, new_name, service_id):
-		"""Renames the service specified by the service ID.
-		
-		Arguments:
-		new_name -- The new service name. This must contain only alpha-
-		            numeric characters, underscores and dashes. The new
-					service name must be different from the current and
-					cannot be a duplicate of another existing service
-					and/or longer than 50 chars.
-		service_id -- The id of the service to be renamed.
-		
-		"""
-		query = { 'method' : 'maintenance.renameService', 'newname' : new_name, 'serviceid' : service_id }
-		return self.api_connection.get(query)
-		
-	# reset a service
-	def reset_service(self, service_id, **kwargs):
-		"""WARNING: This will remove all collected data for a service
-		and is not reversible. The following will be cleared: historical
-		log data, uptime info, service description, URL(s), script(s),
-		search and error strings.
-		
-		Contact info for alerting purposes (as well as error notification
-		settings), monitoring agent locations, maintenance windows, perf-
-		ormance SLA objectives, interval and strikes before error are
-		not affected.
-		
-		The service will be turned off once reset.
-		
-		NOTE: This API is not available for metered accounts.
-		
-		Arguments:
-		service_id -- The id of the service you wish to reset.
-		
-		Keyword Arguments:
-		keep_scripts -- Boolean. Monitoring scripts and URL(s) will not be
-	       	            deleted.
-		keep_description -- Boolean. The service description will not be
-		                    deleted.
-		keep_logs -- Boolean. The log data will not be deleted.
-		
-		"""
-		query = { 'method' : 'maintenance.resetService', 'serviceid' : service_id }
-		if 'keep_scripts' in kwargs and kwargs['keep_scripts'] == True:
-			query.update({ 'keepscripts' : '1' })
-		elif 'keep_description' in kwargs and kwargs['keep_description'] == True:
-			query.update({ 'keepdescription' : '1'})
-		elif 'keep_logs' in kwargs and kwargs['keep_logs'] == True:
-			query.update({ 'keeplogs' : '1' })
-		return self.api_connection.get(query)
-		
-	# duplicate a service
-	# def duplicate_service(self):
-	"""This method is strange. I will come back to it."""
-	
-	# Agent Settings
-	
-	# Service Status
-	
-	# Service Settings
-	
-	# Alert Contact Settings
-	# get all alerting contacts
-	def get_all_alerting_contacts(self, service_id):
-		"""Get all contacts (diagnostic and escalation level) for a service.
-		
-		Arguments:
-		service_id -- The service id of the service you want contacts for.
-		
-		"""
-		query = { 'method' : 'maintenance.getAllAlertingContacts', 'serviceid' : service_id }
-		return self.api_connection.get(query)
-	
-	# Reports
-	
-	# SMS/Voice Contact Settings
-	
-	# SLA Settings
-	
-	# Maintenance Windows
+    def __init__(self, username, api_key):
+        """Initialize Webmetrics' API Client.
+
+        Arguments:
+        username -- The username of the user
+        api_key -- The API key of the user
+
+        """
+        self.api_connection = connection.ApiConnection()
+        self.api_connection.auth(username, api_key)
+        
+    # General
+    # get account services
+    def get_services(self):
+        """Get a list of service names and ids for an account."""
+        query = { 'method' : 'maintenance.getServices' }
+        return self.api_connection.get(query)
+
+    # get account notepad
+    def get_notepad(self):
+        """Returns the notepad of a given account."""
+        query = { 'method' : 'maintenance.getNotepad' }
+        return self.api_connection.get(query)
+
+    # set account notepad
+    def set_notepad(self, notepad):
+        """Sets the notepad of an account.
+        
+        Arguments:
+        notepad -- The notepad contents you wish to set.
+        
+        """
+        query = { 'method' : 'maintenance.setNotepad', 'notepad' : notepad }
+        return self.api_connection.get(query)
+        
+    # get the 'type' of a service by id
+    def get_service_type(self, service_id):
+        """Given a service id, returns the monitoring type.
+        
+        Arguments:
+        service_id -- The id of the service you want to get the 
+                      type of.
+                     
+        """
+        query = { 'method' : 'maintenance.getServiceType', 'serviceid' : service_id }
+        return self.api_connection.get(query)
+        
+    # Admin
+    # create a new monitoring service
+    def add_new_service(self, service_name, service_type):
+        """Add a new monitoring service to the specified Webmetrics
+        account. A trial is created for customers not on metered.
+        
+        Arguments:
+        service_name -- The name to give the new service. Service
+                        names can include alphanumeric, hyphen and
+                        underscore characters.
+        service_type -- The type of service. You can use the following:
+                        APPLICATION
+                        DNS
+                        FTP
+                        PING
+                        POP
+                        PORT
+                        SMTP
+                        STREAM
+                        WEBSERVICE
+                        WEBSITE
+                        WSTRANS		
+
+        """
+        query = { 'method' : 'maintenance.addNewService', 'servicename' : service_name, 'servicetype' : service_type.lower() }
+        return self.api_connection.get(query)
+
+    # change your user password
+    def change_password(self, new_password):
+        """Sets the password for a given username.
+        
+        Arguments:
+        new_password -- The new password.
+        
+        """
+        query = { 'method' : 'maintenance.changePassword', 'newpassword' : new_password }
+        return self.api_connection.get(query)
+
+    # rename an existing service
+    def rename_service(self, new_name, service_id):
+        """Renames the service specified by the service ID.
+        
+        Arguments:
+        new_name -- The new service name. This must contain only alpha-
+                    numeric characters, underscores and dashes. The new
+                    service name must be different from the current and
+                    cannot be a duplicate of another existing service
+                    and/or longer than 50 chars.
+        service_id -- The id of the service to be renamed.
+        
+        """
+        query = { 'method' : 'maintenance.renameService', 'newname' : new_name, 'serviceid' : service_id }
+        return self.api_connection.get(query)
+        
+    # reset a service
+    def reset_service(self, service_id, **kwargs):
+        """WARNING: This will remove all collected data for a service
+        and is not reversible. The following will be cleared: historical
+        log data, uptime info, service description, URL(s), script(s),
+        search and error strings.
+        
+        Contact info for alerting purposes (as well as error notification
+        settings), monitoring agent locations, maintenance windows, perf-
+        ormance SLA objectives, interval and strikes before error are
+        not affected.
+        
+        The service will be turned off once reset.
+        
+        NOTE: This API is not available for metered accounts.
+        
+        Arguments:
+        service_id -- The id of the service you wish to reset.
+        
+        Keyword Arguments:
+        keep_scripts -- Boolean. Monitoring scripts and URL(s) will not be
+                        deleted.
+        keep_description -- Boolean. The service description will not be
+                            deleted.
+        keep_logs -- Boolean. The log data will not be deleted.
+        
+        """
+        query = { 'method' : 'maintenance.resetService', 'serviceid' : service_id }
+        if 'keep_scripts' in kwargs and kwargs['keep_scripts'] == True:
+            query.update({ 'keepscripts' : '1' })
+        elif 'keep_description' in kwargs and kwargs['keep_description'] == True:
+            query.update({ 'keepdescription' : '1'})
+        elif 'keep_logs' in kwargs and kwargs['keep_logs'] == True:
+            query.update({ 'keeplogs' : '1' })
+        return self.api_connection.get(query)
+        
+    # duplicate a service
+    # def duplicate_service(self):
+    """This method is strange. I will come back to it."""
+
+    # Agent Settings
+
+    # Service Status
+
+    # Service Settings
+
+    # Alert Contact Settings
+    # add diagnostic contacts
+    def add_diagnostic_contacts(self, service_id, contacts):
+        """Add diagnostic contacts to a specified service. Diagnostic
+        contacts are verbose and not intended for SMS or pager.
+        
+        Arguments:
+        service_id -- The id of the service to which contacts are being
+                      added.
+        contacts -- A list of email addresses or groups you want to add to
+                    the diagnostic contacts.
+        
+        """
+        if type(contacts) is not list:
+            contacts = [contacts]
+        query = { 'method' : 'maintenance.addDiagnosticContacts', 'serviceid' : service_id, 'contact' : contacts }
+        return self.api_connection.get(query)
+
+    # remove diagnostic contact
+    def remove_diagnostic_contact(self, service_id, contact):
+        """Remove diagnostic contacts from a specified service.
+        
+        Arguments:
+        service_id -- The id of the service for which contacts are being
+                      removed.
+        contact -- A single contact to be removed. Lists are not accepted 
+                   with this method.
+        
+        """
+        query = { 'method' : 'maintenance.removeDiagnosticContact', 'serviceid' : service_id, 'contact' : contact }
+        return self.api_connection.get(query)
+
+    # add escalation contacts
+    def add_escalation_contacts(self, service_id, level, contacts):
+        """Add escalation level contacts to a service. Escalation contacts
+        are formatted for SMS and pager alerts with short messages.
+        
+        Arguments:
+        service_id -- The id of the service to which contacts are being
+                      added.
+        level -- The escalation level you wish to add contacts to. Valid
+                 escalation levels are 1-3.
+        contacts -- A list of email addresses, groups or SMS/voice contacts
+                    you wish to add.
+                    
+        """
+        if type(contacts) is not list:
+            contacts = [contacts]
+        query = { 'method' : 'maintenance.addEscalationLevelContacts', 'serviceid' : service_id, 'level' : level, 'contact' : contacts }
+        return self.api_connection.get(query)
+    
+    # remove escalation contact
+    def remove_escalation_contact(self, service_id, level, contact):
+        """Remove an escalation level contact from a service.
+        
+        Arguments:
+        service_id -- The if of the service for which contacts are being
+                      removed.
+        level -- The service level for removing the specified contact.
+        contacts -- A single contact email to be removed.
+        
+        """
+        query = { 'method' : 'maintenance.removeEscalationLevelContact', 'serviceid' : service_id, 'level' : level, 'contact' : contact }
+        return self.api_connection.get(query)
+
+    # get all alerting contacts
+    def get_all_alerting_contacts(self, service_id):
+        """Get all contacts (diagnostic and escalation level) for a service.
+
+        Arguments:
+        service_id -- The service id of the service you want contacts for.
+
+        """
+        query = { 'method' : 'maintenance.getAllAlertingContacts', 'serviceid' : service_id }
+        return self.api_connection.get(query)
+
+    # Reports
+
+    # SMS/Voice Contact Settings
+
+    # SLA Settings
+
+    # Maintenance Windows
